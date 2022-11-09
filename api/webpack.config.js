d926e4116811058701708a2d4b2900b1716331fb
Wonjun Lee <jasonw0416@gmail.com>
2022-11-08 20:29:45 -0800

    forgot webpack.config.js

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -17,27 +17,27 @@ console.log('environment:::::', environment);
 
 let ENVIRONMENT_VARIABLES = {
   'process.env.HOST': JSON.stringify('localhost'),
-  'process.env.USER': JSON.stringify('*'), //rkuan
-  'process.env.DB': JSON.stringify('*'), //rkuan
+  'process.env.USER': JSON.stringify('wonjunlee'), //rkuan
+  'process.env.DB': JSON.stringify('wonjunlee'), //rkuan
   'process.env.DIALECT': JSON.stringify('postgres'),
   'process.env.PORT': JSON.stringify('3080'),
-  'process.env.PG_CONNECTION_STR': JSON.stringify("*") // "postgres://rkuan:''@localhost:5432/rkuan"
+  'process.env.PG_CONNECTION_STR': JSON.stringify("postgres://wonjunlee:''@localhost:5432/wonjunlee") // "postgres://rkuan:''@localhost:5432/rkuan"
 };
 
 if (environment === 'test') {
   ENVIRONMENT_VARIABLES = {
     'process.env.HOST': JSON.stringify('localhost'),
-    'process.env.USER': JSON.stringify('*'), //rkuan
-    'process.env.DB': JSON.stringify('*'), //rkuan
+    'process.env.USER': JSON.stringify('wonjunlee'), //rkuan
+    'process.env.DB': JSON.stringify('wonjunlee'), //rkuan
     'process.env.DIALECT': JSON.stringify('postgres'),
     'process.env.PORT': JSON.stringify('3080'),
-    'process.env.PG_CONNECTION_STR': JSON.stringify("*") // "postgres://rkuan:''@localhost:5432/rkuan"
+    'process.env.PG_CONNECTION_STR': JSON.stringify("postgres://wonjunlee:''@localhost:5432/wonjunlee") // "postgres://rkuan:''@localhost:5432/rkuan"
   };
 } else if (environment === 'production') {
   ENVIRONMENT_VARIABLES = {
     'process.env.HOST': JSON.stringify('localhost'),
-    'process.env.USER': JSON.stringify('*'), //rkuan
-    'process.env.DB': JSON.stringify('*'), //rkuan
+    'process.env.USER': JSON.stringify('wonjunlee'), //rkuan
+    'process.env.DB': JSON.stringify('wonjunlee'), //rkuan
     'process.env.DIALECT': JSON.stringify('postgres'),
     'process.env.PORT': JSON.stringify('3080'),
     'process.env.PG_CONNECTION_STR': JSON.stringify("postgres://pgadmin@webappdemopostgre:Tester@123@webappdemopostgre.postgres.database.azure.com:5432/tasks")
